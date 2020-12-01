a1e8fd06ee2382c48b8bafff6a682a28145af3dd
chjqkb <chjqkb@users.noreply.github.com>
2020-12-01 23:51:05 +0800

    完成pascals triangle

--- learnstring/src/LearnString.java
+++ learnstring/src/LearnString.java
@@ -26,6 +26,18 @@ public class LearnString {
 
     }
 
+    static int factorial(int n) {
+        int f;
+
+        for(f = 1; n > 1; n--){
+            f *= n;
+        }
+        return f;
+    }
+    static int ncr(int n,int r) {
+        return factorial(n) / ( factorial(n-r) * factorial(r) );
+    }
+
     public static void main(String []args){
         String a = "a";
         String b = "b";
